c6c8a9254204af2d0b127823efcce4bb1454d5cd
Pamela Dayne <pam.dayne@gmail.com>
2021-07-04 01:48:45 +0800

    Improved status and removed unused lines in chatbox.js

--- src/components/chatbox/Chatbox.js
+++ src/components/chatbox/Chatbox.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import css from "./Chatbox.module.css";
 
+
 function Chatbox() {
-  // const [user, setUser] = useState(null);
-
-  // asyn
-
   return (
     <div className={css.chatbox}>
       <p>Log</p>
+
     </div>
   );
 }
