45fe6cbd95ea86ba0305f36fad803ce5a2c17afd
Pamela Dayne <pam.dayne@gmail.com>
2021-07-06 03:32:52 +0800

    Improved BG movement and sizes

--- src/pages/End.js
+++ src/pages/End.js
@@ -4,7 +4,7 @@ import Status from "../components/status/Status";
 
 function End(){
     return <div>
-        <div className={css.sq}></div>
+        <div className={[css.sq, "bx"].join(" ")}></div>
         <Status status="Shutting down" dot="orange"/>
     </div>;
 }
